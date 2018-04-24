5e0f4f2166d851bf0a80411af6ae1d2ca5e308e0
TPaitoon <paitoonjoyparn@gmail.com>
2018-04-24 16:46:34 +0700

    "edit by IT12 : wait edit js"

--- backend/web/js/chkbomscript.js
+++ backend/web/js/chkbomscript.js
@@ -22,6 +22,95 @@ $('#bomsubmit').click(function (e) {
     }
 });
 
+var cntline = 0;
+var empid = $("#cempid");
+var date = $("#cdate");
+var stove = $("#cstoveid");
+var standard = $("#cstandard");
+var hour = $("#chour");
+var amount = $("#camount");
+var losttime = $("#closttime");
+var deduct = $("#cdeduct");
+var totaltire = $("#ctotaltire");
+var perpcs = $("#cperpcs");
+var rate = $("#crate");
+$("#count").text(cntline);
+
+$(".bominfo-form").each(function () {
+    var fBody = $(".bominfo-form").find(".listemp");
+    var fLast = fBody.find("tr:last");
+    var fLaststr = fLast.closest("tr");
+    var fNew;
+
+    $(".adddata", $(this)).click(function () {
+        if (empid.val() !== "") {
+            // alert(fLaster.find(".empids").val());
+            if (fLaststr.find(".empids").val() === "") {
+                fLaststr.find(".empids").val(empid.val());
+                fLaststr.find(".dates").val(date.val());
+                fLaststr.find(".hours").val(hour.val());
+                fLaststr.find(".standards").val(standard.val());
+                fLaststr.find(".stoveids").val(stove.val());
+                fLaststr.find(".amounts").val(amount.val());
+                fLaststr.find(".losttimes").val(losttime.val());
+                fLaststr.find(".deducts").val(deduct.val());
+                fLaststr.find(".totaltires").val(totaltire.val());
+                fLaststr.find(".perpcss").val(perpcs.val());
+                fLaststr.find(".rates").val(rate.val());
+                cntline++;
+                setGroupLine(cntline);
+            } else if (checkGroupValue(empid.val(),date.val(),stove.val()) === 1) {
+
+            }
+        }
+    });
+});
+
+function removegroupline(e) {
+    var fBody = $(".bominfo-form").find(".listemp");
+    var fLast = fBody.find("tr:last");
+    var fLaststr = fLast.closest("tr");
+    if ($("table.listemp >tbody >tr").length > 1) {
+        e.parent().parent().remove();
+        cntline--;
+        setGroupLine(cntline);
+    } else {
+        if (fLaststr.find(".empids").val() !== "") {
+            fLaststr.find(".empids").val("");
+            fLaststr.find(".dates").val("");
+            fLaststr.find(".hours").val("");
+            fLaststr.find(".standards").val("");
+            fLaststr.find(".stoveids").val("");
+            fLaststr.find(".amounts").val("");
+            fLaststr.find(".losttimes").val("");
+            fLaststr.find(".deducts").val("");
+            fLaststr.find(".totaltires").val("");
+            fLaststr.find(".perpcss").val("");
+            fLaststr.find(".rates").val("");
+            cntline--;
+            setGroupLine(cntline);
+        }
+    }
+}
+
+function checkGroupValue(empid, date, stoveid) {
+    var emplist = document.getElementsByName("empidx[]");
+    var datelist = document.getElementsByName("datex[]");
+    var stovelist = document.getElementsByName("stoveidx[]");
+    for (var i = 0; i < emplist.length; i++) {
+        if (empid === emplist[i].value && date === datelist[i].value) {
+            if (stoveid === stovelist[i].value) {
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
+
+function setGroupLine(val) {
+    document.getElementById("count").innerHTML = val;
+}
+
 function getcount() {
     var c = 0;
     var empid = $('#cempid').val();
